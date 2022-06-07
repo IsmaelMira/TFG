d5b4c4b9c10febbaaca9453101c1295826c75b66
Ismael <ismamira20@gmail.com>
2022-06-07 16:11:02 +0200

    7/6

--- ForecastingWeb/public/javascripts/main.js
+++ ForecastingWeb/public/javascripts/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0514c90ffe9d1936b75cb1f437869c5c90797ef6a3abe183a787000bc3a66cb9
-size 2621
+oid sha256:5286d5334c752c4fa8a5feb900e0789afc2d7100463d6d20f74bc5e98ce015c4
+size 3512
--- ForecastingWeb/routes/index.js
+++ ForecastingWeb/routes/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f57551e6f9b65af5da0fad2118347025eba767bf4a8cb24fd7feef4488f5ef65
-size 5310
+oid sha256:35a2f882608332c4bc951bba569fe8c9fb0d07b9049dbdf514563394aef20cce
+size 6017
